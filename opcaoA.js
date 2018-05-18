cf5b4a80151119481631263a3dfc3838aae90608
SaraFe <sara.ferro20@yahoo.com>
2018-05-18 19:39:09 +0200

    Escolher opçao
    menu con le 5 opzioni
    A:2 CH Mode
    B:5 CH Mode
    C:29 CH Mode
    D: Blackout
    E: Exit

--- opcaoA.js
+++ opcaoA.js
@@ -13,14 +13,32 @@
 		
 		//return eval(a);
 	};
+	
+	ext.blackout = function() {
+		for (var i = 1; i <= numChannels; i++) {
+              DMXChannels[i]=0;
+		}
+		
+		//return eval(a);
+	};
+	
+	ext.opcaoA = function(opçaoEscolhida) {
+		opçao=opçaoEscolhida;
+		
+		//return eval(a);
+	};
+	
 	var descriptor = {
 		blocks: [
 			['r', 'Set CH %n to level %n', 'opcaoA'],
+			['', 'Blackout', 'blackout']
+			['', 'Escolher a opção: %m.opçoes','opçao', 'escolher opçao']
 		],
 		
 		menus:{
 			//"channels":	[1,2],
 			//"level":	[0,127,255]
+			"opçoes": [A: 2 CH Mode, B: 5 CH Mode, C: 29 CH Mode, Blackout, Exit]
 		}
 	};
 	ScratchExtensions.register("sACN extension", descriptor, ext);
